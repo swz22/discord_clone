7da7c367818dce45991aa5e8c7ec32fe67bb5010
John Kim <jkim005@yahoo.com>
2023-04-03 17:21:49 -0500

    create chat and chatheader components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Sidebar />
+      <Chat />
     </div>
   );
 }
