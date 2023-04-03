fd8b5511c735af0339857cbeeec2eea028240d2d
John Kim <jkim005@yahoo.com>
2023-04-03 18:59:01 -0500

    implement google authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from "react";
-import "./App.css";
-import Sidebar from "./components/Sidebar";
+import React, { useEffect } from "react";
 import Chat from "./components/Chat";
-import { selectUser } from "./features/userSlice";
-import { useSelector } from "react-redux";
 import Login from "./components/Login";
+import Sidebar from "./components/Sidebar";
+import { selectUser } from "./features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { auth } from "./firebase";
+import { login, logout } from "./features/userSlice";
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //user is logged in
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        //user is logged out
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
 
   return (
     <div className="app">
-    {user ? (
-      <>
-      <Sidebar />
-      <Chat />
-      </>
-    ): (
-      <Login/>
-    )}
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Button } from "@material-ui/core";
+import { auth, provider } from "../firebase";
 import "./Login.css";
 
 function Login() {
   const signIn = () => {
     //google login
+    auth.signInWithPopup(provider).catch((error) => alert(error.message));
   };
 
   return (
