692fbd615ea28cf67755dee0248e4353a3d3c2f0
John Kim <jkim005@yahoo.com>
2023-04-03 16:47:42 -0500

    update sidebar component and styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Sidebar.css";
-import SidebarChannel from "./SidebarChannel";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add";
+import SidebarChannel from "./SidebarChannel";
+import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import CallIcon from "@material-ui/icons/Call";
+import { Avatar } from "@material-ui/core";
+import MicIcon from "@material-ui/icons/Mic";
+import HeadsetIcon from "@material-ui/icons/Headset";
+import SettingsIcon from "@material-ui/icons/Settings";
 
 function Sidebar() {
   return (
@@ -18,7 +25,6 @@ function Sidebar() {
             <ExpandMoreIcon />
             <h4>Text Channels</h4>
           </div>
-
           <AddIcon className="sidebar_addChannel" />
         </div>
         <div className="sidebar_channelsList">
@@ -26,6 +32,34 @@ function Sidebar() {
           <SidebarChannel />
           <SidebarChannel />
           <SidebarChannel />
+        </div>
+      </div>
+
+      <div className="sidebar_voice">
+        <SignalCellularAltIcon className="sidebar_voiceIcon" fontSize="large" />
+
+        <div className="sidebar_voiceInfo">
+          <h3>Voice Connected</h3>
+          <p>Amazing Discord Channel</p>
+        </div>
+
+        <div className="sidebar_voiceIcons">
+          <InfoOutlinedIcon />
+          <CallIcon />
+        </div>
+      </div>
+
+      <div className="sidebar_profile">
+        <Avatar />
+        <div className="sidebar_profileInfo">
+          <h3>@swz22</h3>
+          <p>#sampleID</p>
+        </div>
+
+        <div className="sidebar_profileIcons">
+          <MicIcon />
+          <HeadsetIcon />
+          <SettingsIcon />
         </div>
       </div>
     </div>
