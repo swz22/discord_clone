f992c55be8ea298874262c8006c7c448b92eb08e
John Kim <jkim005@yahoo.com>
2023-04-03 16:13:31 -0500

    create sidebar and sidebar channel components

---
+++ src/components/Sidebar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import SidebarChannel from "./SidebarChannel";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar_top">
+        <h3>Amazing Discord Channel</h3>
+        <ExpandMoreIcon />
+      </div>
+
+      <div className="sidebar_channels">
+        <div className="sidebar_channelsHeader">
+          <div className="sidebar_header">
+            <ExpandMoreIcon />
+            <h4>Text Channels</h4>
+          </div>
+
+          <AddIcon className="sidebar_addChannel" />
+        </div>
+        <div className="sidebar_channelsList">
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/components/SidebarChannel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function SidebarChannel({id, channel}) {
+  return <div className="sidebar-channel">
+  <h4><span className='sidebarChannel_hash'>#</span>Placeholder</h4>
+
+  </div>;
+}
+
+export default SidebarChannel
